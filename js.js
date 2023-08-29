9708d49ca43f7881b660747b70e8215479005f3c
unknown <Baxtiyorddushaev@gmail.com>
2023-08-29 22:35:01 +0500

    js.indets

--- js.js
+++ js.js
@@ -2,16 +2,180 @@
 
 
 
-let age = +prompt('Yoshingizni kiriting !');
+// let age = +prompt('Yoshingizni kiriting !');
 
-if (age > 0 && age <= 18 && !isNaN(age)) {
-  console.log('Your are very yong. You are must to study');
-} else if (age > 18 && age <= 50 && !isNaN(age)) {
-  console.log('Your are must to go to job');
-} else if (age > 50 && age <= 59 && !isNaN(age)) {
-  console.log('You will go to pension');
-} else if (age > 59 && age <= 150 && !isNaN(age)) {
-  console.log('Your are a pensioneer');
-} else {
-  console.log('something is wrong');
-}
+// if (age > 0 && age <= 18 && !isNaN(age)) {
+//   console.log('Your are very yong. You are must to study');
+// } else if (age > 18 && age <= 50 && !isNaN(age)) {
+//   console.log('Your are must to go to job');
+// } else if (age > 50 && age <= 59 && !isNaN(age)) {
+//   console.log('You will go to pension');
+// } else if (age > 59 && age <= 150 && !isNaN(age)) {
+//   console.log('Your are a pensioneer');
+// } else {
+//   console.log('something is wrong');
+// }
+
+
+// let a = +prompt('Write first number'),
+//   b = +prompt('Write second number'),
+//   c = +prompt('Write third number');
+
+//   if (a > b && a < c) {
+//     console.log(a);
+//   } else if (b > a && b < c) {
+//     console.log(b);
+//   } else if (c > a && c < b) {
+//     console.log(c);
+//   } else {
+//     console.log('Something is wrong');
+//   }
+
+// let num = +prompt('Write clock');
+
+// switch(num) {
+//   case 1:
+//   alert("clock is"  + "1:00" + "!");
+//   break;
+  
+//   case 2:
+//   alert("clock is"  + "2:00" + "!");
+//   break;
+  
+//   case 3:
+//   alert("clock is"  + "3:00" + "!");
+//   break;
+  
+//   case 4:
+//   alert("clock is"  + "4:00" + "!");
+//   break;
+  
+//   case 5:
+//   alert("clock is"  + "5:00" + "!");
+//   break;
+  
+//   case 6:
+//   alert("clock is"  + "6:00" + "!");
+//   break;
+  
+//   case 7:
+//   alert("clock is"  + "7:00" + "!");
+//   break;
+  
+//   case 8:
+//   alert("clock is"  + "8:00" + "!");
+//   break;
+  
+//   case 9:
+//   alert("clock is"  + "9:00" + "!");
+//   break;
+  
+//   case 10:
+//   alert("clock is"  + "10:00" + "!");
+//   break;
+  
+//   case 11:
+//   alert("clock is"  + "11:00" + "!");
+//   break;
+  
+//   case 12:
+//   alert("clock is"  + "12:00" + "!");
+//   break;
+  
+//   case 13:
+//   alert("clock is"  + "13:00" + "!");
+//   break;
+  
+//   case 14:
+//   alert("clock is"  + "14:00" + "!");
+//   break;
+  
+//   case 15:
+//   alert("clock is"  + "15:00" + "!");
+//   break;
+  
+//   case 16:
+//   alert("clock is"  + "16:00" + "!");
+//   break;
+  
+//   case 17:
+//   alert("clock is"  + "17:00" + "!");
+//   break;
+  
+//   case 18:
+//   alert("clock is"  + "18:00" + "!");
+//   break;
+  
+//   case 19:
+//   alert("clock is"  + "19:00" + "!");
+//   break;
+  
+//   case 20:
+//   alert("clock is"  + "20:00" + "!");
+//   break;
+  
+//   case 21:
+//   alert("clock is"  + "21:00" + "!");
+//   break;
+  
+//   case 22:
+//   alert("clock is"  + "22:00" + "!");
+//   break;
+  
+//   case 23:
+//   alert("clock is"  + "23:00" + "!");
+//   break;
+  
+//   case 24:
+//   alert("clock is"  + "24:00" + "!");
+//   break;
+//   default: 
+//   alert('Something is wrong');  
+// } 
+
+// let num = +prompt('Write number');
+
+// num = !isNaN(num) ? num : 10;
+
+// alert(num)
+
+// if(!isNaN(num) ){
+//   num = num 
+// }else {
+//   num = 10;
+// }
+
+// alert(num)
+
+// let number = +prompt('Write number!'),
+//   stepen = +prompt('Write second number'),
+//   answer = 1;
+
+//   for(let i = 0; i < stepen; i++) {
+//     answer = answer * number
+//   }
+
+//   alert(answer);
+
+
+let amount = +prompt('Enter the number of steps'),
+  symbol = prompt('Enter the number of indets'),
+  last = prompt('Last symbol');
+
+  for (let i = 0; i < amount; i++) {
+    if (i == 0 ) {
+      console.log(last);
+    } else {
+      console.log(last = symbol + last);
+    }
+  }
+
+
+
+
+
+
+
+
+
+
